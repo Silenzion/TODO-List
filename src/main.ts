42f5e7eeab2a65c73b3b3974aed607d34dbc2729
Svetlana Dmitrieva <53534598+Silenzion@users.noreply.github.com>
2022-04-17 14:05:18 +0300

    Added pinia.

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,8 @@
 import { createApp } from "vue";
+import { createPinia } from "pinia";
 import "@/assets/styles/index.scss";
 import App from "@/App.vue";
 
-const app = createApp(App);
+const app = createApp(App).use(createPinia());
 
 app.mount("#app");
