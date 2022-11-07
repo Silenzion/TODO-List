7d69024d1844e6391a9332050c3081c7b7b4c844
Svetlana Dmitrieva <53534598+Silenzion@users.noreply.github.com>
2022-11-07 21:24:46 +0300

    fix config

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,7 @@ import OptimizationPersist from "vite-plugin-optimize-persist";
 import PkgConfig from "vite-plugin-package-config";
 
 export default defineConfig({
-  base: "https://silenzion.github.io/To-Do-Vue3-ts-/",
+  base: "'./'",
   plugins: [vue(), OptimizationPersist(), PkgConfig()],
   resolve: {
     alias: {
