878e4dfbdacc47ba865ac7f173a3121e09eac98e
Svetlana Dmitrieva <53534598+Silenzion@users.noreply.github.com>
2023-06-10 22:34:35 +0300

    Reinstall eslint, fix test

--- tests/todoItem/renderItem.spec.ts
+++ tests/todoItem/renderItem.spec.ts
@@ -4,20 +4,29 @@ import { beforeEach, describe, expect, test } from "vitest";
 import TodoItem from "../../src/assets/components/TodoItem.vue";
 import TodoItemModel from "../../src/assets/models/TodoItem";
 
+
 describe("renderTodoItem",()=>{
+  const DESC = "Tests is fine!";
+
   beforeEach(() => {
     setActivePinia(createPinia())
   });
+
   const model = new TodoItemModel({
-    description:
-"Tests is fine!"
+    description: DESC
   });
+
+
   test("simpleRender", ()=>{
     const wrapper = mount(TodoItem,{
       props:{
         model: model
       }
     });
-    expect(wrapper.find('.todo-item')).toHaveLength(1);
+    const todo = wrapper.get('.todo-item');
+    const title = wrapper.get('.todo-item__desc')
+
+    expect(title.text()).toBe(DESC);
+    expect(wrapper.findAll('.todo-item')).toHaveLength(1);
   });
 });
