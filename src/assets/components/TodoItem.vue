341033d9bacdd57be7a3fe5743d412e9c181ecd9
Svetlana Dmitrieva <53534598+Silenzion@users.noreply.github.com>
2023-06-18 11:57:24 +0300

    Test for TodoItem.vue is updated

--- src/assets/components/TodoItem.vue
+++ src/assets/components/TodoItem.vue
@@ -2,6 +2,7 @@
 import TodoItem from "@/assets/models/TodoItem";
 import {ETodoItemState} from "@/assets/models/ETodoItemState.enum";
 import {useMainStore} from "@/assets/store";
+import {computed, StyleValue} from "vue";
 
 const props = defineProps<{
   model: TodoItem
@@ -20,17 +21,24 @@ const setStateIsDone = (): void => {
 const setStateIsTodo = (): void => {
   store.setItemState(props.model.id, ETodoItemState.TODO);
 };
+
+const classes = computed<Record<string, boolean>>(()=>{
+  return {
+    "todo-item_done":props.model.isDone,
+    "todo-item_todo":props.model.isToDo
+  }
+});
 </script>
 <template>
-    <div class="todo-item">
+    <div class="todo-item" :class="classes">
       <div class="todo-item__content">
         <div class="todo-item__content-left">
-          <button v-if="model.isDone" class="base-button base-button_success" @click="setStateIsTodo">
+          <button v-if="model.isDone" class="todo-item__todo-button base-button base-button_success" @click="setStateIsTodo">
             <i class="fa fa-check" aria-hidden="true"/>
           </button>
           <button
             v-if="model.isToDo"
-            class="base-button check-button text-gray-600"
+            class="todo-item__done-button base-button check-button text-gray-600"
             @click="setStateIsDone"
           >
             <i class="fa fa-check transition-all" aria-hidden="true"/>
@@ -39,7 +47,7 @@ const setStateIsTodo = (): void => {
             {{ model.description }}
           </div>
         </div>
-        <button class="base-button base-button_danger" @click="deleteElement">
+        <button class="todo-item__delete-button base-button base-button_danger" @click="deleteElement">
           <i class="fa fa-trash-o" aria-hidden="true"/>
         </button>
       </div>
--- tests/TodoItem.spec.ts
+++ tests/TodoItem.spec.ts
@@ -2,36 +2,54 @@ import { mount } from "@vue/test-utils";
 import { createPinia, setActivePinia } from "pinia";
 import { beforeEach, describe, expect, test } from "vitest";
 import TodoItem from "../src/assets/components/TodoItem.vue";
+import { ETodoItemState } from "../src/assets/models/ETodoItemState.enum";
 import TodoItemModel from "../src/assets/models/TodoItem";
 
+const TEST_TEXT = "Tests is fine!"
 
-describe("TodoItem",()=>{
-  const DESC = "Tests is fine!";
-
-  const model = new TodoItemModel({
-    description: DESC
-  });
+describe("TodoItem.vue",()=>{
 
   beforeEach(() => {
     setActivePinia(createPinia())
   });
 
-
-
-
-  test("render", ()=>{
-    const wrapper = mount(TodoItem,{
-      props:{
-        model: model
-      }
-    });
-    const title = wrapper.get('.todo-item__desc')
-
-    expect(title.text()).toBe(DESC);
+  test("render test", ()=>{
+    const wrapper = mount(TodoItem,{props:new TodoItemModel()});
     expect(wrapper.findAll('.todo-item')).toHaveLength(1);
+    expect( wrapper.get('.todo-item__desc').text()).toBe("");
+
   });
 
-  test("states",()=>{
+  test("todo state",()=>{
+    const todoTask = new TodoItemModel({
+      description: TEST_TEXT,
+      state: ETodoItemState.TODO
+    });
+    const wrapper = mount(TodoItem,{
+      props:{
+        model: todoTask
+      }
+    });
+    const doneBtn = wrapper.find('.todo-item__done-button');
+    const todoBtn = wrapper.find('.todo-item__todo-button');
+    expect(wrapper.classes()).contain("todo-item_todo");
+    expect(todoBtn.exists()).toBe(false);
+    expect(doneBtn.exists()).toBe(true);
+  });
 
+  test("done state",()=>{
+    const doneTask = new TodoItemModel({
+      description: TEST_TEXT
+    });
+  // TODO:дописать
+  });
+
+  test('Delete', async () => {
+    const wrapper = mount(TodoItem,{props:{model: new TodoItemModel()}})
+    const deleteBtn = wrapper.find('.todo-item__delete-button')
+    expect(deleteBtn.exists()).toBe(true);
+
+    await deleteBtn.trigger('click');
+    expect(wrapper.exists()).toBe(false);
   })
 });
