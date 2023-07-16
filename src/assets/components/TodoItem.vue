ba408d83542490a864435d2cf5efb86f22da30e7
Svetlana Dmitrieva <53534598+Silenzion@users.noreply.github.com>
2023-07-16 18:23:41 +0300

    fix

--- src/assets/components/TodoItem.vue
+++ src/assets/components/TodoItem.vue
@@ -1,14 +1,11 @@
 <script setup lang="ts">
 import TodoItem from "@/assets/models/TodoItem";
-import {ETodoItemState} from "@/assets/models/ETodoItemState.enum";
-import {useMainStore} from "@/assets/store";
 import {computed} from "vue";
 
-const emit = defineEmits(['delete','setDone','setToDo'])
+const emit = defineEmits(['delete', 'setDone', 'setToDo'])
 const props = defineProps<{
   model: TodoItem
 }>();
-
 
 const emitDeleteElement = async (): Promise<void> => {
   emit('delete');
@@ -22,40 +19,40 @@ const emitSetStateIsTodo = (): void => {
   emit('setToDo');
 };
 
-const classes = computed<Record<string, boolean>>(()=>{
+const classes = computed<Record<string, boolean>>(() => {
   return {
-    "todo-item_done":props.model.isDone,
-    "todo-item_todo":props.model.isToDo
+    "todo-item_done": props.model.isDone,
+    "todo-item_todo": props.model.isToDo
   }
 });
 </script>
 <template>
-    <div class="todo-item" :class="classes">
-      <div class="todo-item__content">
-        <div class="todo-item__content-left">
-          <button v-if="model.isDone" class="todo-item__todo-button base-button base-button_success" @click="emitSetStateIsTodo">
-            <i class="fa fa-check" aria-hidden="true"/>
-          </button>
-          <button
-            v-if="model.isToDo"
-            class="todo-item__done-button base-button check-button text-gray-600"
-            @click="emitSetStateIsDone"
-          >
-            <i class="fa fa-check transition-all" aria-hidden="true"/>
-          </button>
-          <div class="todo-item__desc" :class="{ 'todo-item__desc_done': model.isDone }">
-            {{ model.description }}
-          </div>
-        </div>
-        <button class="todo-item__delete-button base-button base-button_danger" @click="emitDeleteElement">
-          <i class="fa fa-trash-o" aria-hidden="true"/>
+  <div class="todo-item" :class="classes">
+    <div class="todo-item__content">
+      <div class="todo-item__content-left">
+        <button v-if="model.isDone" class="todo-item__todo-button base-button base-button_success" @click="emitSetStateIsTodo">
+          <i class="fa fa-check" aria-hidden="true"/>
         </button>
+        <button
+          v-if="model.isToDo"
+          class="todo-item__done-button base-button check-button text-gray-600"
+          @click="emitSetStateIsDone"
+        >
+          <i class="fa fa-check transition-all" aria-hidden="true"/>
+        </button>
+        <div class="todo-item__desc" :class="{ 'todo-item__desc_done': model.isDone }">
+          {{ model.description }}
+        </div>
       </div>
+      <button class="todo-item__delete-button base-button base-button_danger" @click="emitDeleteElement">
+        <i class="fa fa-trash-o" aria-hidden="true"/>
+      </button>
     </div>
+  </div>
 </template>
 
 <style lang="scss" scoped>
-.todo-item{
+.todo-item {
   margin-bottom: 2.4rem;
   width: 100%;
 
@@ -66,7 +63,7 @@ const classes = computed<Record<string, boolean>>(()=>{
     justify-content: space-between;
     padding-top: 0.5rem;
 
-    &-left{
+    &-left {
       display: flex;
       align-items: center;
     }
@@ -77,13 +74,14 @@ const classes = computed<Record<string, boolean>>(()=>{
     margin-left: 1.2rem;
     text-align: left;
 
-    &_done{
+    &_done {
       text-decoration: line-through;
     }
 
   }
 }
-.check-button{
+
+.check-button {
   .fa-check {
     opacity: 0;
   }
